1405c110c8b8a97bf9ad2555f5e5f8c1c9b6abf8
Sepero <cowpie2000@hotmail.com>
2013-08-11 19:04:25 -0400

    Fix bug with Freenode

--- auto_ghost.py
+++ auto_ghost.py
@@ -1,26 +1,30 @@
 __module_name__ = "Auto Ghost"
-__module_version__ = "0.1.0"
-__module_description__ = "Ghost the default nick and change to it."
+__module_version__ = "0.1.1"
+__module_description__ = "Ghost the default nick and change/identify as that user."
 
-print "\0034",__module_name__, __module_version__,"has been loaded\003"
+print("\0034",__module_name__, __module_version__,"has been loaded\003")
 
 import xchat
-import time
+from time import sleep
 
 def on_server_connected(word, word_eol, userdata):
-    chan = xchat.get_context()
     desired_nick = xchat.get_prefs("irc_nick1")
     current_nick = xchat.get_info("nick")
     password = xchat.get_info("nickserv")
     
-    if password:
+    if desired_nick and password:
         if current_nick != desired_nick:
             xchat.command("msg nickserv ghost %s %s" % (desired_nick, password))
-            time.sleep(1)
+            sleep(1)
             xchat.command("nick %s" % desired_nick)
-            time.sleep(1)
-    
+            sleep(1)
+        
         xchat.command("msg nickserv identify %s" % password)
+        
+        # Need to set nick again for some unknown reason on Freenode.
+        if current_nick != desired_nick:
+          sleep(1)
+          xchat.command("nick %s" % desired_nick)
     
     return xchat.EAT_NONE
 
