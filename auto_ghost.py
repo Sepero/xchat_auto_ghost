e973fed52988f8ca31665dcf21698d388a708dab
Sepero <cowpie2000@hotmail.com>
2013-08-27 10:21:11 -0400

    Large simplification of code

--- auto_ghost.py
+++ auto_ghost.py
@@ -1,37 +1,31 @@
+from random import choice
+import string
 import xchat
-from time import sleep
 
 __module_name__ = "Auto Ghost"
-__module_version__ = "0.13"
+__module_version__ = "0.20"
 __module_description__ = "Ghost the default nick and change/identify as that user."
 
 display = (__module_name__ + " " + __module_version__ + " has been loaded.",
-        "Module URL: https://github.com/Sepero/xchat_auto_ghost/",
-        "Author: Sepero - sepero 111 @ gmail . com",
-        " Remote Python developer and Linux administrator for hire.",)
+        "    Module URL: https://github.com/Sepero/xchat_auto_ghost/",
+        "    Author: Sepero 2013 - sepero 111 @ gmail . com",
+        "         Remote Python developer and Linux administrator for hire.",)
 
 for line in display:
     print("\0034" + line + "\003")
 
-def on_server_connected(word, word_eol, userdata):
+def set_nick(word, word_eol, userdata):
     desired_nick = xchat.get_prefs("irc_nick1")
-    current_nick = xchat.get_info("nick")
     password = xchat.get_info("nickserv")
     
-    if desired_nick and password:
-        if current_nick != desired_nick:
-            xchat.command("msg nickserv ghost %s %s" % (desired_nick, password))
-            sleep(1)
-            xchat.command("nick %s" % desired_nick)
-            sleep(1)
-        
-        xchat.command("msg nickserv identify %s" % password)
-        
-        # Need to set nick again for some unknown reason on Freenode.
-        if current_nick != desired_nick:
-          sleep(1)
-          xchat.command("nick %s" % desired_nick)
+    if word[3] == desired_nick and password:
+        randstring = ''.join(choice(string.lowercase) for i in xrange(16))
+        xchat.command("nick %s" % randstring)
+        xchat.command("nickserv ghost %s %s" % (desired_nick, password))
+        xchat.command("nick %s" % desired_nick)
+        xchat.command("nickserv identify %s" % password)
+        return xchat.EAT_XCHAT
     
     return xchat.EAT_NONE
 
-xchat.hook_server("255", on_server_connected)
+xchat.hook_server("433", set_nick, priority=xchat.PRI_HIGHEST)
